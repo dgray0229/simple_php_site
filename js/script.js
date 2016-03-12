0e09d6f7393d8b0ef12d740aa7ecc62f7a511164
Devin G <devin.gray92@gmail.com>
2016-03-12 12:58:44 -0800

    changed script to switch statement instead of if

--- js/script.js
+++ js/script.js
@@ -4,23 +4,26 @@ var $moviesFormat = $('#movies').hide();
 toggleFields();
 
 function toggleFields() {
+
   $('#format optgroup').hide();
   $('#genre optgroup').hide();
 
-  if ($("#category").val() == "Books") {
-    $('#format optgroup#books').show();
-    $('#genre optgroup#books').show();
+
+  switch (true) {
+    case ($("#category").val() == "Books"):
+      $('#format optgroup#books').show();
+      $('#genre optgroup#books').show();
+      break;
+    case ($("#category").val() == "Movies"):
+      $('#format optgroup#movies').show();
+      $('#genre optgroup#movies').show();
+      break;
+    case ($("#category").val() == "Music"):
+      $('#format optgroup#music').show();
+      $('#genre optgroup#music').show();
+      break;
   }
 
-  if ($("#category").val() == "Movies") {
-    $('#format optgroup#movies').show();
-    $('#genre optgroup#movies').show();
-  }
-
-  if ($("#category").val() == "Music") {
-    $('#format optgroup#music').show();
-    $('#genre optgroup#music').show();
-  }
 }
 
 $("#category").on("change", toggleFields);
